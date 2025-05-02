887da79a6e9ba61125f4cbdfca1c253125e8cb71
JJJHANG <jangzhu5678@gmail.com>
2025-05-02 16:07:21 +0800

    fix: vite config for images

--- frontend/src/components/BackgroundAnimation.jsx
+++ frontend/src/components/BackgroundAnimation.jsx
@@ -4,16 +4,28 @@ const BackgroundAnimation = () => {
     return (
         <div className="bg-ani-fixed">
             <div className="cirg1">
-                <img src="/images/cir1.png" alt="" />
+                <img
+                    src={`${import.meta.env.BASE_URL}images/cir1.png`}
+                    alt=""
+                />
             </div>
             <div className="cirg2">
-                <img src="/images/cir2.png" alt="" />
+                <img
+                    src={`${import.meta.env.BASE_URL}images/cir2.png`}
+                    alt=""
+                />
             </div>
             <div className="cirg3">
-                <img src="/images/cir3.png" alt="" />
+                <img
+                    src={`${import.meta.env.BASE_URL}images/cir3.png`}
+                    alt=""
+                />
             </div>
             <div className="cirg4">
-                <img src="/images/cir4.png" alt="" />
+                <img
+                    src={`${import.meta.env.BASE_URL}images/cir4.png`}
+                    alt=""
+                />
             </div>
         </div>
     );
